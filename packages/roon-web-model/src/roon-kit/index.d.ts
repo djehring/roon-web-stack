f7f8be8c5f21d9bad48e9422b324eb1b474a725d
David Jehring <david@blackpear.com>
2026-09-17 15:29:28 +0100

    fix: remove duplicate AI search response declaration

--- packages/roon-web-model/src/roon-kit/index.d.ts
+++ packages/roon-web-model/src/roon-kit/index.d.ts
@@ -5,7 +5,6 @@
  New typings might be added to integrate missing part of the roon API in the future
  */
 import { OutputDescription } from "../api-model";
-import { SuggestedTrack } from "../client-model";
 
 export type EmptyObject = {
   [K in unknown]: never;
@@ -146,10 +145,6 @@ export interface RoonApiBrowseLoadResponse {
   items: Item[];
   offset: number;
   list: List;
-}
-
-export interface AISearchResponse {
-  items: SuggestedTrack[];
 }
 
 export interface RoonApiBrowse {
